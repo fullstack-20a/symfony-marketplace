ac3a9d258169770780cd2b013997423a18b7ba21
LH <lh@applh.com>
2020-07-21 14:32:42 +0200

    ajout js lightbox

---
+++ public/assets/js/main.js
@@ -0,0 +1,38 @@
+console.log("main.js chargé...");
+
+// CODE JS POUR LIGHTBOX
+// ON VA AJOUTER UNE ACTION SUR LES IMAGES DANS LES ARTICLES
+
+function ajouterAction (selecteurCSS, evenementJS, callback)
+{
+    var listeBalise = document.querySelectorAll(selecteurCSS);
+    for(var b=0; b<listeBalise.length; b++)
+    {
+        var balise = listeBalise[b];
+        balise.addEventListener(evenementJS, callback);
+    }
+}
+
+function afficherLightbox (event)
+{
+    console.log(event.target);
+
+    // AFFICHER LA LIGHTBOX AVEC IMAGE EN GRAND DEDANS
+    var baliseLightbox = document.querySelector(".lightbox");
+    if (baliseLightbox != null)
+    {
+        var imgGrand = baliseLightbox.querySelector("img");
+        imgGrand.src = event.target.src;
+        baliseLightbox.classList.add('afficher');
+    }
+}
+function cacherLightbox (event)
+{
+    var baliseLightbox = document.querySelector(".lightbox");
+    if (baliseLightbox != null)
+    {
+        baliseLightbox.classList.remove('afficher');
+    }
+}
+ajouterAction("article img", "click", afficherLightbox);
+ajouterAction(".lightbox", "click", cacherLightbox);
